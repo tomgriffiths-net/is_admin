767c8214b9d65e04a5a930ae1aafb86297a98f9d
Tom <tomgriffiths2007@gmail.com>
2025-06-10 18:01:58 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,39 +1,23 @@
 <?php
-//Your Settings can be read here: settings::read('myArray/settingName') = $settingValue;
-//Your Settings can be saved here: settings::set('myArray/settingName',$settingValue,$overwrite = true/false);
 class is_admin{
-    //public static function command($line):void{}//Run when base command is class name, $line is anything after base command (string). e.g. > [base command] [$line]
+    private static $state = null;
     public static function init():void{
         if(self::check()){
-            mklog("general","Starting with administrator permissions",false);
+            mklog(1,"Starting with administrator permissions");
         }
-    }//Run at startup
+    }
     public static function check():bool{
-        
-        if(isset($GLOBALS['is_admin']['check_state'])){
-            if(is_bool($GLOBALS['is_admin']['check_state'])){
-                return $GLOBALS['is_admin']['check_state'];
-            }
+        if(!is_bool(self::$state)){
+            mklog(0,"Checking for Administrator privilages");
+            exec("net session >nul 2>&1",$output,$resultCode);
+
+            self::$state = ($resultCode === 0);
         }
 
-        $result = false;
-
-        mklog('general',"Checking for Administrator privilages");
-        exec("net session >nul 2>&1",$output,$resultCode);
-
-        if($resultCode === 0){
-            $result = true;
-        }
-
-        $GLOBALS['is_admin']['check_state'] = $result;
-
-        return $result;
-
+        return self::$state;
     }
     public static function refresh():bool{
-        if(isset($GLOBALS['is_admin']['check_state'])){
-            unset($GLOBALS['is_admin']['check_state']);
-        }
+        self::$state = null;
         return self::check();
     }
 }
